f7d42e09cb67c15366237c63fd2e4a2b1ee55b0e
dmsgh0304 <dmsgh0304@gmail.com>
2026-02-22 00:36:39 +0900

    Update login.c

--- code/login.c
+++ code/login.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    char id[20];
+    int password;
+
+    // 저장된 계정 정보
+    int correct_password = 1234;
+
+    printf("아이디를 입력하세요: ");
+    scanf("%s", id);
+
+    printf("비밀번호를 입력하세요: ");
+    scanf("%d", &password);
+
+    // 로그인 확인
+    if (password == correct_password) {
+        printf("로그인 성공!\n");
+        printf("환영합니다, %s 님.\n", id);
+    } else {
+        printf("로그인 실패! 비밀번호가 틀렸습니다.\n");
+    }
+
+    return 0;
+}
